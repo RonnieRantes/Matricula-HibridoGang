f2c0d0a6f769a0378eb1ba48a4cda1f2c1303ffc
RonnieRantes <rantesroni1@gmail.com>
2022-04-04 19:57:58 -0500

    upd

--- src/main/java/pe/edu/upc/spring/controller/EstudianteController.java
+++ src/main/java/pe/edu/upc/spring/controller/EstudianteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EstudianteController {
 
 	@RequestMapping("/")
-	public String irMenuu(Model model) {
+	public String irMenu(Model model) {
 		return "menu";
 	}
 }
