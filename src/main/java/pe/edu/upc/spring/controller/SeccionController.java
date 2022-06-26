716e2c6cb422e0d79e96614a8cd153a41c872a12
RonnieRantes <rantesroni1@gmail.com>
2022-06-26 18:23:12 -0500

    fix seccion

--- src/main/java/pe/edu/upc/spring/controller/SeccionController.java
+++ src/main/java/pe/edu/upc/spring/controller/SeccionController.java
@@ -19,7 +19,6 @@ import pe.edu.upc.spring.service.IDocenteService;
 
 @Controller
 @RequestMapping("/seccion")
-
 public class SeccionController {
     @Autowired
     private ISeccionService sService;
