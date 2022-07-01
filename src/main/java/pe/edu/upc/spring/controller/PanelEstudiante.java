5d518709b8ef13145c8a8d438fbd68406009c43a
RonnieRantes <rantesroni1@gmail.com>
2022-07-01 15:01:26 -0500

    Update PanelEstudiante.java

--- src/main/java/pe/edu/upc/spring/controller/PanelEstudiante.java
+++ src/main/java/pe/edu/upc/spring/controller/PanelEstudiante.java
@@ -80,7 +80,7 @@ public class PanelEstudiante {
             		s.setVacantes(mService.vacantesSeccion(s.getCodigo(), semestre));
             		s.setHorario("Lunes - Viernes");
             		s.setMatriculado(mService.comprobarSeccion(s.getCodigo(), semestre, logeado.getName(), idCurso));
-					Systen.out.println("COMPRO MAT: " + s.getMatriculado());
+					System.out.println("COMPRO MAT: " + s.getMatriculado());
             	}
             	model.addAttribute("listaSecciones", lst);
         		model.addAttribute("matricula", objM);
